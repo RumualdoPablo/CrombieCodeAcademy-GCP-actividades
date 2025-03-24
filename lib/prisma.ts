0fa99146c82dec83dabc4f056d76316d57f92bfd
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-03-24 19:13:00 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const databaseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL_CLOUD
-    : process.env.DATABASE_URL;
-
-export const prisma = new PrismaClient({
-  datasources: {
-    db: { url: databaseUrl },
-  },
-});
-
-console.log(databaseUrl);
+export const prisma = new PrismaClient({});
