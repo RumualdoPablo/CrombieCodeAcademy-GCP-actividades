3fc5b6dbb08147884407b1f85e8111e4bfaa9c3d
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-03-26 16:25:19 -0300

    Update page.tsx

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -43,7 +43,7 @@ export default function Products() {
                 </div>
                 <h1 className="text-4xl font-bold text-center mb-8 text-blue-600">Our Products</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {products.map((product) => (
+                    {products?.map((product) => (
                         <div key={product.productId} className="bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 transition-transform">
                             <div className="p-4">
                                 <Image src={product.image} alt={product.name} height={200} width={200} />
